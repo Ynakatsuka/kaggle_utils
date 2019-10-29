9ca4cdc2ecddee089c2e918a727402db5fe2c4d5
Yuki Nakatsuka <nk.tsssa@gmail.com>
2019-10-30 00:58:56 +0900

    added visualizations

--- kaggle_utils/visualizations/__init__.py
+++ kaggle_utils/visualizations/__init__.py
@@ -104,10 +104,54 @@ def plot_mean_feature_importances(feature_importances, max_num=50, importance_ty
 
 
 def plot_prediction_histogram(y, bins=100, path=None):
-    # plot
     if path is not None:
         plt.clf()
         plt.figure(figsize=(12, 6))
         plt.hist(y, bins=bins)
         plt.title('Histogram')
         plt.savefig(path)
+
+
+def plot_actual_difference(df, pred, target, col, topn=50, path=None):
+    train = df[[col, target]].copy()
+    train['__pred'] = pred
+    nunique = train[col].nunique()
+    if nunique > topn:
+        keys = train[col].value_counts().iloc[:topn].index
+        train = train[train[col].isin(keys)]
+
+    plt.clf()
+    fig = plt.figure(figsize=(16, 4))
+    ax1 = fig.add_subplot(111)    
+    ax2 = ax1.twinx()
+    
+    g = train.groupby(col)
+    m = g[['__pred', target]].mean()
+    if isinstance(list(g.groups.keys())[0], str):
+        keys = [k for k in g.groups.keys()]
+    else:
+        keys = [f'{k:.2f}' for k in g.groups.keys()]
+    ax2.bar(keys, g[target].count(), align='center', color='gray', alpha=0.5)
+    ax1.plot(keys, m['__pred'], marker='.', markersize=14)
+    ax1.plot(keys, m[target], marker='.', markersize=14)
+    ax1.set_xticklabels(keys, rotation=45)
+    ax1.set_xlabel(col)
+    ax1.set_ylabel('Mean target')
+    ax1.grid(True)
+    ax2.set_ylabel('Frequency')
+    ax2.set_ylim(0, g[target].count().max()*3)
+    if path is not None:
+        plt.savefig(path)
+
+
+def plot_lift_chart(true, pred, bins=50, path=None):
+    plt.clf()
+    df = pd.DataFrame({'pred': pred, 'true': true}).sort_values('pred')
+    chunk_size = np.ceil(len(df) / bins)
+    df['bin'] = np.floor(np.arange(1, 1+len(df)) / chunk_size)
+    df.groupby('bin')[['pred', 'true']].mean().plot(marker='.', markersize=14, figsize=(16, 4))
+    plt.grid()
+    plt.title('Lift Chart')
+    if path is not None:
+        plt.savefig(path)
+        
