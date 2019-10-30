5f2774f867668590c391884df30e84b1c76a20ce
Yuki Nakatsuka <nk.tsssa@gmail.com>
2019-10-31 01:42:02 +0900

    added plot all

--- kaggle_utils/visualizations/__init__.py
+++ kaggle_utils/visualizations/__init__.py
@@ -1,18 +1,47 @@
 import itertools
-import numpy as np
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
 
+def plot_all(task_type, df, model, true, pred, bins=50, base_path='./', name='', bot=None,
+             pred_true_difference_features=None, classes=None, importance_type='gain', importance_path=None, predictors=None):
+    to_send = {}
+    if (importance_path is not None) and (predictors is not None):
+        imp = get_importances_from_model(model, predictors, importance_type=importance_type)
+        imp.to_pickle(importance_path)
+        plot_feature_importances(imp, max_num=bins, importance_type=importance_type, path=base_path+f'{name}_importance_{importance_type}.png')
+        to_send[importance_type] = base_path+f'{name}_importance_{importance_type}.png'
+
+    if task_type in ['binary', 'regression']:
+        plot_prediction_histogram(pred, bins=bins, path=base_path+f'{name}_histogram.png')
+        to_send['histogram'] = base_path+f'{name}_histogram.png'
+        if (importance_path is not None) and (predictors is not None):
+            pred_true_difference_features += list(imp.sort_values(importance_type, ascending=False)['feature'].iloc[:3])
+        for feature in pred_true_difference_features:
+            plot_pred_true_difference(df, true, pred, feature, topn=bins, path=base_path+f'{name}_pred_true_difference_per_{feature}.png')
+            to_send[f'pred_true_difference_per_{feature}'] = base_path+f'{name}_pred_true_difference_per_{feature}.png'
+        plot_lift_chart(true, pred, bins=bins, path=base_path+f'{name}_lift_chart.png')
+        to_send['lift_chart'] = base_path+f'{name}_lift_chart.png'
+    elif task_type == 'multiclass':
+        plot_confusion_matrix(true, pred, classes, path=base_path+f'{name}_confusion_matrix.png')
+        to_send['lift_chart'] = base_path+f'{name}_lift_chart.png'
+    else:
+        print('Unsupported task type.')
+    if bot is not None:
+        for message, path in to_send.items():
+            bot.send(message, image=path)
+
+
 def plot_confusion_matrix(true, pred, classes,
                           normalize=True,
-                          title='Confusion matrix',
-                          path=None,
-                          cmap=plt.cm.Blues):
+                          cmap=plt.cm.Blues,
+                          path=None):
     '''
     Args
     ----
@@ -30,7 +59,7 @@ def plot_confusion_matrix(true, pred, classes,
     plt.clf()
     plt.figure(figsize=(12, 12))
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
+    plt.title('Confusion matrix')
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
@@ -80,75 +109,75 @@ def get_importances_from_model(model, predictors, importance_type='gain'):
 
 def plot_feature_importances(importances, max_num=50, importance_type='gain', path=None):    
     importances = importances.sort_values(importance_type, ascending=False).iloc[:max_num]
-    # plot
+
+    plt.clf()
+    plt.figure(figsize=(16, 6))
+    sns.barplot(x=importance_type, y='feature', data=importances, orient='h')
+    plt.title(importance_type)
+    plt.tight_layout()
     if path is not None:
-        plt.clf()
-        plt.figure(figsize=(16, 6))
-        sns.barplot(x=importance_type, y='feature', data=importances, orient='h')
-        plt.title(importance_type)
-        plt.tight_layout()
         plt.savefig(path)
         
 
 def plot_mean_feature_importances(feature_importances, max_num=50, importance_type='gain', path=None):
     mean_gain = feature_importances[[importance_type, 'feature']].groupby('feature').mean()
     feature_importances['mean_'+importance_type] = feature_importances['feature'].map(mean_gain[importance_type])
-
-    if path is not None:
-        data = feature_importances.sort_values('mean_'+importance_type, ascending=False).iloc[:max_num]
-        plt.clf()
-        plt.figure(figsize=(16, 6))
-        sns.barplot(x=importance_type, y='feature', data=data)
-        plt.tight_layout()
-        plt.savefig(path)
-
-
-def plot_prediction_histogram(y, bins=100, path=None):
-    if path is not None:
-        plt.clf()
-        plt.figure(figsize=(12, 6))
-        plt.hist(y, bins=bins)
-        plt.title('Histogram')
-        plt.savefig(path)
-
-
-def plot_actual_difference(df, pred, target, col, topn=50, path=None):
-    train = df[[col, target]].copy()
-    train['__pred'] = pred
-    nunique = train[col].nunique()
-    if nunique > topn:
-        keys = train[col].value_counts().iloc[:topn].index
-        train = train[train[col].isin(keys)]
+    data = feature_importances.sort_values('mean_'+importance_type, ascending=False).iloc[:max_num]
 
     plt.clf()
-    fig = plt.figure(figsize=(16, 4))
+    plt.figure(figsize=(16, 6))
+    sns.barplot(x=importance_type, y='feature', data=data)
+    plt.tight_layout()
+    if path is not None:
+        plt.savefig(path)
+
+
+def plot_prediction_histogram(pred, bins=100, path=None):
+    plt.clf()
+    plt.figure(figsize=(12, 6))
+    plt.hist(pred, bins=bins)
+    plt.title('Histogram')
+    if path is not None:
+        plt.savefig(path)
+
+
+def plot_pred_true_difference(dataframe, true, pred, col, topn=50, path=None):
+    df = pd.DataFrame({'true': true, 'pred': pred, col: dataframe[col]})
+    nunique = df[col].nunique()
+    if nunique > topn:
+        keys = df[col].value_counts().iloc[:topn].index
+        df = df[df[col].isin(keys)]
+
+    plt.clf()
+    fig = plt.figure(figsize=(16, 6))
     ax1 = fig.add_subplot(111)    
     ax2 = ax1.twinx()
     
-    g = train.groupby(col)
-    m = g[['__pred', target]].mean()
+    g = df.groupby(col)
+    m = g[['pred', 'true']].mean()
     if isinstance(list(g.groups.keys())[0], str):
         keys = [k for k in g.groups.keys()]
     else:
         keys = [f'{k:.2f}' for k in g.groups.keys()]
-    ax2.bar(keys, g[target].count(), align='center', color='gray', alpha=0.5)
-    ax1.plot(keys, m['__pred'], marker='.', markersize=14)
-    ax1.plot(keys, m[target], marker='.', markersize=14)
+    ax2.bar(keys, g['true'].count(), align='center', color='gray', alpha=0.5)
+    ax1.plot(keys, m['pred'], marker='.', markersize=14)
+    ax1.plot(keys, m['true'], marker='.', markersize=14)
     ax1.set_xticklabels(keys, rotation=45)
     ax1.set_xlabel(col)
     ax1.set_ylabel('Mean target')
     ax1.grid(True)
     ax2.set_ylabel('Frequency')
-    ax2.set_ylim(0, g[target].count().max()*3)
+    ax2.set_ylim(0, g['true'].count().max()*3)
+    plt.title(f'predicted values v.s. true values per {col}')
     if path is not None:
         plt.savefig(path)
 
 
 def plot_lift_chart(true, pred, bins=50, path=None):
-    plt.clf()
     df = pd.DataFrame({'pred': pred, 'true': true}).sort_values('pred')
     chunk_size = np.ceil(len(df) / bins)
     df['bin'] = np.floor(np.arange(1, 1+len(df)) / chunk_size)
+    plt.clf()
     df.groupby('bin')[['pred', 'true']].mean().plot(marker='.', markersize=14, figsize=(16, 4))
     plt.grid()
     plt.title('Lift Chart')
