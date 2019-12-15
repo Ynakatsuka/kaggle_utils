ba7e7c47ceb107a19afc319de116cd3efad62a79
Yuki Nakatsuka <nk.tsssa@gmail.com>
2019-12-16 02:17:58 +0900

    revised utils

--- kaggle_utils/utils/__init__.py
+++ kaggle_utils/utils/__init__.py
@@ -1,23 +1,52 @@
-import functools
-import gc
+from contextlib import contextmanager
 import json
+from logging import getLogger, Formatter, StreamHandler, FileHandler, INFO
+from multiprocessing import Pool
+import os
 import requests
 import time
-import numpy as np
+
 from IPython.display import display, Javascript
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils.validation import check_is_fitted, column_or_1d
+
 from .sampling import *
 from .load import *
 
 
-def timer(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print('elapsed time: {:.1f} s'.format(end - start))
-        return result
-    return wrapper
+def init_logger(file='log.txt'):
+    dirs = '/'.join(file.split('/')[:-1])
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+    
+    LOGFORMAT = '%(asctime)s %(levelname)s %(message)s'
+    logger = getLogger()
+    fh_handler = FileHandler(file)
+    fh_handler.setFormatter(Formatter(LOGFORMAT))
+    logger.setLevel(INFO)
+    logger.addHandler(fh_handler)
+    return logger
+
+
+@contextmanager
+def timer(name, logger=None):
+    if logger is not None:
+        logger.info(f'[{name}] start')
+    t0 = time.time()
+    yield
+    if logger is not None:
+        logger.info(f'[{name}] done in {time.time() - t0:.0f} s')
+
+
+def parallelize_dataframe(df, func, n_workers=4):
+    df_split = np.array_split(df, n_workers)        
+    pool = Pool(n_workers)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
 
 
 def check_path(path):
@@ -27,29 +56,22 @@ def check_path(path):
     
 def save_notebook(current_ipynb_path, save_ipynb_path):
     print('Saving notebook...')
-    display(Javascript("IPython.notebook.save_notebook()"), include=['application/javascript'])
+    display(Javascript('IPython.notebook.save_notebook()'), include=['application/javascript'])
     time.sleep(3)
     command = 'jupyter nbconvert --to notebook {} --output {}'.format(current_ipynb_path, save_ipynb_path)
     print('executing {}...'.format(command))
     os.system(command)
     print('Done.')   
-    
-
-# def submit(competition_name, submit_path, message=''):
-#     command = 'kaggle competitions submit -c {} -f {} -m {}'.format(competition_name, submit_path, message)
-#     os.system(command)
 
 
 def submit(competition_name, file_path, comment='from API'):
-    os.system(f'kaggle competitions submit -c {competition_name} -f {file_path} -m "{comment}"')
+    os.system(f'kaggle competitions submit -c {competition_name} -f {file_path} -m '{comment}'')
     time.sleep(60)
     tmp = os.popen(f'kaggle competitions submissions -c {COMPETITION_NAME} -v | head -n 2').read()
     col, values = tmp.strip().split('\n')
     message = 'SCORE!!!\n'
     for i,j in zip(col.split(','), values.split(',')):
         message += f'{i}: {j}\n'
-#        print(f'{i}: {j}') # TODO: comment out later?
-#     send_line(message.rstrip())
 
 
 class LINENotifyBot():
@@ -110,7 +132,6 @@ def change_dtype(dataframe, columns=None):
                     dataframe[col] = dataframe[col].astype(np.float32)
                 else:
                     dataframe[col] = dataframe[col].astype(np.float64)
-    gc.collect()
     return dataframe
 
 
@@ -119,6 +140,35 @@ def to_category(train, cat=None):
         cat = [col for col in train.columns if train[col].dtype == 'object']
     for c in cat:
         train[c], uniques = pd.factorize(train[c])
-        maxvalue_dict[c] = train[c].max() + 1
-    gc.collect()
     return train
+
+
+class TolerantLabelEncoder(LabelEncoder):
+    def __init__(self, ignore_unknown=True):
+        self.ignore_unknown = ignore_unknown
+
+    def transform(self, X, y=None):
+        check_is_fitted(self, 'classes_')
+        X = column_or_1d(X, warn=True)
+
+        indices = np.isin(X, self.classes_)
+        if not self.ignore_unknown and not np.all(indices):
+            raise ValueError('X contains new labels: %s' 
+                                         % str(np.setdiff1d(X, self.classes_)))
+
+        X_transformed = np.searchsorted(self.classes_, X)
+        X_transformed[~indices] = len(self.classes_)
+        return X_transformed
+
+    def inverse_transform(self, X):
+        check_is_fitted(self, 'classes_')
+
+        labels = np.arange(len(self.classes_))
+        indices = np.isin(X, labels)
+        if not self.ignore_unknown and not np.all(indices):
+            raise ValueError('X contains new labels: %s' 
+                                         % str(np.setdiff1d(X, self.classes_)))
+
+        X_transformed = np.asarray(self.classes_[X], dtype=object)
+        X_transformed[~indices] = 'unknown'
+        return X_transformed
