246253ca91b3993420722c07464c38a3b823f3a7
Ynakatsuka <nk.tsssa@gmail.com>
2020-02-11 16:18:57 +0900

    removed unused files

--- kaggle_utils/preprocess/__init__.py
+++ kaggle_utils/preprocess/__init__.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-from scipy.special import erfinv
 from scipy.stats import boxcox
 from sklearn.base import BaseEstimator, TransformerMixin
-# from .preprocess import 
 
 
 def to_category(dataframe, cat=None):
@@ -12,33 +10,6 @@ def to_category(dataframe, cat=None):
     for c in cat:
         dataframe[c], uniques = pd.factorize(dataframe[c])
     return dataframe
-
-
-class GaussRankScaler(BaseEstimator, TransformerMixin):
-    def __init__(self, epsilon=0.001):
-        self.epsilon = epsilon
-        self.lower = -1 + self.epsilon
-        self.upper =  1 - self.epsilon
-        self.range = self.upper - self.lower
-
-    def transform(self, X, y=None):
-        i = np.argsort(X, axis=0)
-        j = np.argsort(i, axis=0)
-
-        assert (j.min()==0).all()
-        assert (j.max()==len(j) - 1).all()
-        
-        j_range = len(j) - 1
-        self.divider = j_range / self.range
-        
-        transformed = j / self.divider
-        transformed = transformed - self.upper
-        transformed = erfinv(transformed)
-        
-        return transformed
-    
-    def fit_transform(self, X, y=None):
-        return self.transform(X)
 
         
 class BoxCoxScaler(BaseEstimator, TransformerMixin):
