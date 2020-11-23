71ec6d50266c51578d7389ef8907f1412bde98ea
Yuki Nakatsuka <nk.tsssa@gmail.com>
2020-11-23 17:00:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ def _requires_from_file(filename):
 setup(
     name='kaggle_utils',
     version='0.0.1',
+    packages=find_packages(),
     install_requires=_requires_from_file('requirements.txt'),
     description='utility scripts for kaggle',
     author='Yuki Nakatsuka',
